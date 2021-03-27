df4296ed7ccd39707c68db69d1dcae92ca24c249
WillianFarias <williansousafarias@gmail.com>
2021-03-27 14:55:23 -0300

    pag de cadastro

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React from "react";
 import './App.css';
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <h1>Product Registration</h1>
+
+      <div className="form">
+        <input type="text" name="productName" />
+        <input type="text" name="productDescrition" />
+      </div>
+
+    </div>
+  );
 }
 
 export default App;
