405e1ec8fe36149390b1e38978979dffe2a2cf63
WillianFarias <williansousafarias@gmail.com>
2021-03-27 17:35:17 -0300

    get

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
+import Axios from 'axios';
 
 function App() {
   return (
     <div className="App">
+
+      const [movieName, setMovieName] = useState("");
+      const [review, setReview] = useState("");
+
+      const submitReview = () => {
+        Axios.post('http://localhost:3001/')
+      };
+
       <h1>Product Registration</h1>
 
       <div className="form">
         <label>Name</label>
-        <input type="text" name="productName" />
+        <input type="text" name="movieName" onChange={
+          (e) => {
+            setMovieName(e.target.value)
+          }} />
 
         <label>Description</label>
-        <input type="text" name="productDescrition" />
 
-        <button>Submit</button>
+        <input
+          type="text"
+          name="review"
+          onChange={(e) => {
+            setReview(e.target.value);
+          }}
+        />
+
+        <button onClick={submitReview}>Submit</button>
       </div>
 
     </div>
