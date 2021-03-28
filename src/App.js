542006106d15f241655d754f24c4049b855c68d2
WillianFarias <williansousafarias@gmail.com>
2021-03-27 21:00:47 -0300

    reestruturando

--- src/App.js
+++ src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Films Register</h1>
+
+      <div className="form">
+        <input type="text" name="movieName"/>
+        <input type="text" name="review"/>
+      </div>
     </div>
   );
 }
